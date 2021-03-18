9261c5db2f206f0307a5c5807cf9a00b70fb0d11
Cspidar <cspidar@gmail.com>
2021-03-18 16:58:01 +0900

    convert test-4

--- api/ascii_test.js
+++ api/ascii_test.js
@@ -2,7 +2,7 @@ module.exports = (req, res) => {
   const Asciidoctor = require('asciidoctor')
   const asciidoctor = Asciidoctor();
 
-  var html = asciidoctor.convertFile('https://asciidoctor-test-hlof4636q-cspidar.vercel.app/test.adoc', { to_file: false, standalone: true })
+  var html = asciidoctor.convert(fs.readFileSync('/path/to/file.adoc'))
   // var html = asciidoctor.convert('Hello, _Asciidoctor_')
   // console.log(html)
 
