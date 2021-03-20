1fc4ff52fb01f26fcc1fb16a5e8ff52799bc5f82
cspidar <32433348+cspidar@users.noreply.github.com>
2021-03-20 17:39:00 +0900

    table test

--- api/ascii_test.js
+++ api/ascii_test.js
@@ -3,7 +3,27 @@ module.exports = (req, res) => {
   const asciidoctor = Asciidoctor();
 
   // var html = asciidoctor.convertFile('/doc/test.adoc', { to_file: false, standalone: true })
-  var html = asciidoctor.convert('Hello, _Asciidoctor_')
+  var html = asciidoctor.convert(
+    
+//:hardbreaks:    
+    
+`[options="header, autowidth",format=csv,frame=topbot]
+|===
+include::address1.csv[]
+|===
+
+[options="header, autowidth",format=csv,frame=topbot]
+[cols="~, ~", options="header, autowidth",frame=topbot]
+|===
+|표시 |설명
+|1 |설정 영역 지우기
+|2 |붓 영역 그리기
+|===
+`
+
+
+    
+    )
   // console.log(html)
 
 
